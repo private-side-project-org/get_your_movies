3bce7ec82528ec375579f045453efd4ce3359e9d
gonakano <g.nakano555@gmail.com>
2022-04-07 01:36:05 +0900

    implemented toast message

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ToastContainer } from "react-toastify";
 import Movies from "./Movies/MoviesContent";
 
+import "react-toastify/dist/ReactToastify.css";
 import "assets/styles.scss";
 
 const App = () => {
   const client = new QueryClient();
+  const toastOptions = {
+    position: "top-right",
+    autoClose: 1000,
+    hideProgressBar: true,
+    closeOnClick: true,
+    draggable: false,
+  };
   return (
     <QueryClientProvider client={client}>
       <Movies />
+      <ToastContainer {...toastOptions} />
     </QueryClientProvider>
   );
 };
